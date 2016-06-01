35dc64e5d1bb1b66c14184688f8583d360542fb2
yuhuaxingyun <soukou@wanting.local>
2016-06-01 09:36:39 +0800

    Model
    第一版

---
+++ XHongShu/Model/Destination.m
@@ -0,0 +1,22 @@
+//
+//  Destination.m
+//
+//  Created by  万霆 on 16/5/30
+//  Copyright (c) 2016 万霆. All rights reserved.
+//
+
+#import "Destination.h"
+#import "Banners.h"
+
+
+@implementation Destination
+
++(BOOL)propertyIsOptional:(NSString *)propertyName{
+    return YES;
+}
+
++(JSONKeyMapper *)keyMapper
+
+{
+    return [[JSONKeyMapper alloc] initWithDictionary:nil];
+}@end
